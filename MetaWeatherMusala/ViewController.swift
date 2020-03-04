d84d00b3bafad4bc0f9f2bb725749ae1c1fdbb6a
Ivan Jovanovik <jovanovikj.ivan@gmail.com>
2020-03-04 11:59:20 +0100

    Initial commit

---
+++ MetaWeatherMusala/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetaWeatherMusala
+//
+//  Created by Ivan Jovanovik on 3/4/20.
+//  Copyright © 2020 Ivan Jovanovik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
